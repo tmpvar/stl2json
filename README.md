bb79af51a9209e0a2aafca09e5faa0083d5461e9
Elijah Insua <tmpvar@gmail.com>
2014-03-01 10:09:52 -0800

    functional

--- README.md
+++ README.md
@@ -1,7 +1,41 @@
-## stl2json
+# stl2json
 
+Convert binary|ascii stl data into json on the command line
 
+## install
 
+`npm install -g stl2json`
+
+## usage
+
+### process stdin by default
+
+```cat file | stl2json```
+
+```
+$ cat ~/Downloads/ISO_SHAFT_CLAMP.stl | stl2json
+{"normal":[0.9948693513870239,9.842677430607844e-16,0.10116831958293915],"verts":[[3.261598825454712,-4.953000068664551,0.6597403287887573] ...
+```
+
+### read from file 
+
+```stl2json <file>```
+
+```
+$ stl2json /Users/tmpvar/Downloads/ISO_SHAFT_CLAMP.stl
+{"normal":[0.9948693513870239,9.842677430607844e-16,0.10116831958293915],"verts":[[3.261598825454712,-4.953000068664551,0.6597403287887573] ...
+```
+
+### flatten
+
+```stl2json -f```
+
+If you are only interested in vertex data, this is the flag for you.
+
+```
+$ cat ~/Downloads/ISO_SHAFT_CLAMP.stl | stl2json -f
+[3.261598825454712,-4.953000068664551,0.6597403287887573,3.261598825454712,-4.055254936218262,0.6597403287887573,3.251199960708618,
+```
 
 ### license
 
--- stl2json.js
+++ stl2json.js
@@ -1,12 +1,47 @@
-var stl2json = function() {
-  
+#!/usr/bin/env node
+
+var stl = require('stl');
+var argv = require('optimist').argv;
+var through = require('through2');
+var path = require('path');
+var fs = require('fs');
+
+var parser = stl.createParseStream();
+var source = process.stdin;
+if (argv._.length) {
+  console.log(path.resolve(argv._[0]));
+  source = require('fs').createReadStream(
+    path.resolve(argv._[0])
+  );
 }
 
+var first = true;
 
-if (typeof module !== "undefined" && typeof module.exports == "object") {
-  module.exports = stl2json;
-}
+source
+  .pipe(parser)
+  .pipe(through.obj(function(obj, enc, fn) {
+    if (obj && obj.verts) {
+      if (first) {
+        this.push('[');
+        first = false;
+      } else {
+        this.push(',');
+      }
 
-if (typeof window !== "undefined") {
-  window.stl2json = window.stl2json || stl2json;
-}
+      // Flatten
+      if (argv.f) {
+        this.push(obj.verts.map(function(vert) {
+          return vert.join(',')
+        }).join(','))
+
+      } else {
+        this.push(JSON.stringify(obj));
+      }
+    }
+    fn();
+  }))
+  .pipe(process.stdout);
+
+parser.on('end', function() {
+  process.stdout.write(']');
+});
